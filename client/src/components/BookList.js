2dd354ab7b12ff990a38bc1fb6a23bdc4df638be
praveen-me <praveenkrsaini0@gmail.com>
2019-01-16 23:07:09 +0530

    Add style to app

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import {graphql} from 'react-apollo';
 import { getBooksQuery } from '../queries/query';
+import BookDetails from './BookDetails';
 
 
 
 class BookList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null
+    }
+  }
+  
+  handleClick = e => {
+    this.setState({
+      selected: e.target.id
+    });
+  }
+
   displayBooks() {
     const {loading, books} = this.props.data
     if(loading) {
       return <p>Loading...</p>
     } else {
       return books && books.map(book => (
-        <li key={book.id}>{book.name}</li>
+        <li key={book.id} id={book.id} onClick={this.handleClick}>{book.name}</li>
       ))
     }
   }
@@ -25,6 +39,7 @@ class BookList extends Component {
           this.displayBooks()
         }
         </ul>
+        <BookDetails selectedBook={this.state.selected}/>
       </div>
     );
   }
